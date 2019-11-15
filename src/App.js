7df26bcb4c2e2f56c4eccef919445ae854d8620b
Steven Gerard <stevengerard@Stevens-MacBook-Pro.local>
2019-11-14 20:15:22 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import data from './components/data'
+import Card from './components/Card'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(props){
+    super(props);
+
+    this.state = {
+      people: data,
+      i: 0
+    }
+  }
+
+  handleClick = (e) => {
+    if (e.target.name === 'next') {
+      this.setState({
+        i: this.state.i + 1
+      })
+    } else {
+      this.setState({
+        i: this.state.i - 1
+      })
+    }
+  }
+
+  render() {
+    return (
+      <body className="App">
+        <header className='app-header'>
+          <p className="container">HOME</p>
+        </header>
+        <div className='card'>
+          <Card person={this.state.people[this.state.i]}/>
+        </div>
+        <div className='footer container'>
+          <button><i className="fas fa-chevron-left"></i>Previous</button>
+          <button>Next<i className="fas fa-chevron-right"></i></button>
+        </div>
+      </body>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Card.js
@@ -0,0 +1,32 @@
+import React, {Component} from 'react';
+import './Card.css';
+
+class Card extends Component {
+    constructor(props){
+        super(props);
+            
+            this.state = {
+
+            }
+    }
+
+    render(){
+        let person = this.props.person
+        return(
+            <div className='card'>
+                <div className='container card-content'>
+                    <p className='name'>{person.name.first} {person.name.last}</p>
+                    <p className='index'>Index</p>
+                    <p className='info'><strong>City: </strong>{person.city}</p>
+                    <p className='info'><strong>Country: </strong>{person.country}</p>
+                    <p className='info'><strong>Employer: </strong>{person.employer}</p> 
+                    <p><strong>Title: </strong>{person.title}</p>
+                    <p><strong>Favorite Movie: </strong></p>
+        {person.favoriteMovies.map((e, i) => <p className="favorite-movie" key={e}>{i+1}. {e}</p>)}
+                </div>
+            </div>
+            
+        )
+    }
+}
+export default Card;
